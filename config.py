51755bf84597096c977f29e66b81afbaa403b0e4
aabilatharzeen7 <81191254+aabilatharzeen7@users.noreply.github.com>
2023-07-05 13:36:57 -0500

    results

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # projectpath = r"C:\Users\aabil\PycharmProjects\tbi_biomarker_identification"
 # projectpath = r"C:\Users\aabilatharzeen\PycharmProjects\tbi_biomarker_identification"
 projectpath = r"C:\Users\aabilatharzeen\PycharmProjects\tbi_biomarker_identification\combined"
@@ -8,4 +9,18 @@ datapath = projectpath + "\\data\\"
 modelpath = projectpath +"\\models\\"+ "\\time_1\\"
 
 
+=======
+# projectpath = r"C:\Users\aabil\PycharmProjects\tbi_biomarker_identification"
+# projectpath = r"C:\Users\aabilatharzeen\PycharmProjects\tbi_biomarker_identification"
+# projectpath = r"C:\Users\aabilatharzeen\PycharmProjects\tbi_biomarker_identification\combined"
+projectpath = r"C:\Users\aabil\PycharmProjects\tbi_biomarker_identification\combined"
+# projectpath = r"C:\Users\aabilatharzeen\PycharmProjects\tbi_biomarker_identification\round_2"
+
+datapath = projectpath + "\\data\\"
+
+modelpath = projectpath +"\\models\\"+ "\\time_1\\"
+
+
+
+>>>>>>> Stashed changes
 resultspath = projectpath + "\\results\\"
